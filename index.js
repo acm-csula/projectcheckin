f227cad21eaeb01df156b16120cd0abda8abbbd0
srivats <srivats.venkataraman@gmail.com>
2019-09-30 13:29:16 -0700

    added a if-else method in index.js to make sure the input is not empty

--- index.js
+++ index.js
@@ -4,35 +4,39 @@ function spaceinvaders(){
 
 	var spaceinvadersCIN = document.getElementById("spaceInvadersMemberCIN").value;
 	var spaceInvadersName = document.getElementById("spaceInvadersMemberName").value;
-	var spaceInvadersSecretCode = document.getElementById("spaceInvadersSecretCode").value
+	var spaceInvadersSecretCode = document.getElementById("spaceInvadersSecretCode").value;
 
-	db.collection("Space Invaders").doc().set({
-		name:spaceInvadersName,
-		cin:spaceinvadersCIN,
-		code:spaceInvadersSecretCode
-	})
-	.then(function(docRef) {
-		window.alert("Check In Successful")
-	})
-	.catch(function(error) {
-		window.alert("Please try again")
-	});
+	if(spaceInvadersName.length && spaceinvadersCIN.length && spaceInvadersSecretCode.length != 0){
+		db.collection("Space Invaders").doc().set({
+			name:spaceInvadersName,
+			cin:spaceinvadersCIN,
+			code:spaceInvadersSecretCode
+		})
+		.then(function(docRef) {
+			window.alert("Check In Successful")
+		})	
+	}
+	else{
+		window.alert("All fields are required")
+	}
 }
 
 function accord(){
 	var accordMemberCIN = document.getElementById("accordMemberCIN").value;
 	var accordMemberName = document.getElementById("accordMemberName").value;
-	var accordSecretCode = document.getElementById("accordSecretCode").value
-	
-	db.collection("Accord").doc().set({
-		name:accordMemberName,
-		cin:accordMemberCIN,
-		code:accordSecretCode
-	})
-	.then(function(docRef) {
-		window.alert("Check In Successful")
-	})
-	.catch(function(error) {
-		window.alert("Please try again")
-	});
+	var accordSecretCode = document.getElementById("accordSecretCode").value;
+
+	if(accordMemberName.length && accordMemberCIN && accordSecretCode != 0){
+		db.collection("Accord").doc().set({
+			name:accordMemberName,
+			cin:accordMemberCIN,
+			code:accordSecretCode
+		})
+		.then(function(docRef) {
+			window.alert("Check In Successful")
+		})
+	}
+	else{
+		window.alert("All fields are required")
+	}
 }
