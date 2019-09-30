3e9f0b4a0bd4468dd5e3eb826ee8fefca5ac2edf
srivats <srivats.venkataraman@gmail.com>
2019-09-29 19:18:21 -0700

    added a new field

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function spaceinvaders(){
 
 	var spaceinvadersCIN = document.getElementById("spaceInvadersMemberCIN").value;
 	var spaceInvadersName = document.getElementById("spaceInvadersMemberName").value;
+	var spaceInvadersSecretCode = document.getElementById("spaceInvadersSecretCode").value
 
 	db.collection("Space Invaders").doc().set({
 		name:spaceInvadersName,
-		cin:spaceinvadersCIN
+		cin:spaceinvadersCIN,
+		code:spaceInvadersSecretCode
 	})
 	.then(function(docRef) {
 		window.alert("Check In Successful")
@@ -20,10 +22,12 @@ function spaceinvaders(){
 function accord(){
 	var accordMemberCIN = document.getElementById("accordMemberCIN").value;
 	var accordMemberName = document.getElementById("accordMemberName").value;
+	var accordSecretCode = document.getElementById("accordSecretCode").value
 	
 	db.collection("Accord").doc().set({
 		name:accordMemberName,
-		cin:accordMemberCIN
+		cin:accordMemberCIN,
+		code:accordSecretCode
 	})
 	.then(function(docRef) {
 		window.alert("Check In Successful")
